cf7353cac7f3e650af31ee6cfe210bcc35861bdc
levando20194017 <levando194017@gmail.com>
2023-07-12 15:51:47 +0700

    build FE

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,34 @@
+import { Header } from "./components/Header";
+import { useEffect } from "react";
+import "./App.css";
+
+function App() {
+  //...render
+  // calling API
+  // array dependencies
+
+  let x = 1;
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+  }, [])
+
+  return (
+    <div className="app">
+      <Header />
+      <div className="app-content">
+        <h1>App</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+/**
+ * where to call API (API call is asynchronous code )
+ * 1. inside component
+ * 2. tools: redux-saga, redux-toolkit, react-query
+ * Component: pure function
+ * Side effect
+ */
